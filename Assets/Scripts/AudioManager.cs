a3c6611c5a61571e73206efef8a0f1c2a642c75c
natecurtiss <n8dev.cs@gmail.com>
2022-07-17 09:47:17 -0400

    Add audio management

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PieceCombat
+{
+    class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+        [SerializeField] int _maxAudioSources = 20;
+        readonly Queue<AudioSource> _audioSources = new();
+
+        void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                Init();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Init()
+        {
+            for (var i = 0; i < _maxAudioSources; i++)
+            {
+                var go = new GameObject($"AudioSource_{i}");
+                go.transform.SetParent(transform);
+                var aud = go.AddComponent<AudioSource>();
+                aud.playOnAwake = false;
+                aud.loop = false;
+                aud.spatialBlend = 1f;
+                _audioSources.Enqueue(aud);
+            }
+        }
+
+        public void PlaySound(AudioClip sound, Vector3 position, float volume = 1f)
+        {
+            if (_audioSources.Count == 0)
+                return;
+            var aud = _audioSources.Dequeue();
+            aud.clip = sound;
+            aud.pitch = Random.Range(0.9f, 1.1f);
+            aud.volume = volume;
+            aud.transform.position = position;
+            aud.Play();
+            StartCoroutine(ResetAudioSource(aud, sound.length));
+        }
+
+        IEnumerator ResetAudioSource(AudioSource aud, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            _audioSources.Enqueue(aud);
+        }
+    }
+}
---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PieceCombat
+{
+    class PlaySound : MonoBehaviour
+    {
+        [SerializeField] AudioClip _sound;
+        [SerializeField] float _volume = 1f;
+
+        public void Do() => AudioManager.Instance.PlaySound(_sound, transform.position, _volume);
+    }
+}
